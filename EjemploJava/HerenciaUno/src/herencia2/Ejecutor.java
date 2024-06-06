fd29fc1cb2c741e8770a2edaf1d0f5f42fbdafc3
NicolayJ <dnjimenez6@utpl.edu.ec>
2024-06-06 17:49:20 -0500

    Listoso =D

---
+++ EjemploJava/HerenciaUno/src/herencia2/Ejecutor.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia2;
+import herencia1.EstudiantePresencial;
+/**
+ *
+ * @author reroes
+ */
+public class Ejecutor {
+    
+    public static void main(String[] args) {
+        // Objeto de tipo Estudiante Presencial
+        EstudiantePresencial e1 = new EstudiantePresencial();
+        
+        e1.establecerNombresEstudiante("René Rolando");
+        e1.establecerApellidoEstudiante("Elizalde Solano");
+        e1.establecerIdentificacionEstudiante("1104111111");
+        e1.establecerEdadEstudiante(38);
+        e1.establecerNumeroCreditos(30);
+        e1.establecerCostoCredito(15);
+        e1.calcularMatriculaPresencial();
+        
+        System.out.printf("Nombres: %s\n"
+                + "Apellidos: %s\n"
+                + "Identificacion: %s\n"
+                + "Edad: %d\n"
+                + "Numero de creditros: %d\n"
+                + "Costo de creditos: %.1f\n"
+                + "Costo de matricula: %.1f\n",
+                e1.obtenerNombresEstudiante(),
+                e1.obtenerApellidoEstudiante(),
+                e1.obtenerIdentificacionEstudiante(),
+                e1.obtenerEdadEstudiante(),
+                e1.obtenerNumeroCreditos(),
+                e1.obtenerCostoCredito(),
+                e1.obtenerMatriculaPresencial());
+        
+    }
+}
